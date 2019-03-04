50f02a71e726121786a25680f961f0f8254bbb62
Pawel Butra <pawel.butra@onet.pl>
2019-03-04 13:16:19 +0100

    Zadanie 5

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '929610951933-bt1g75k3rohv2ea2nt5o1rqgeofnv20b.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '2E1sDwrmpjT57pH5Wyn-Dm5P',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
